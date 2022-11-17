5b65fb5bfa8c6be6e139145c600b71eb5f1c2323
Vanessa Emerick <vavarooks123@gmail.com>
2022-11-17 10:29:42 -0800

    More comment edits

--- client/src/views/ViewAll.jsx
+++ client/src/views/ViewAll.jsx
@@ -8,7 +8,8 @@ const ViewAll = () => {
     const [loaded, setLoaded] = useState(false);
     const [comment, setComment] = useState([
         "Wow This Sucks!",
-        "This Rocks!"
+        "This Rocks!",
+        "Love this image!"
     ])
 
     const deleteChatter = (id) => {
@@ -33,32 +34,35 @@ const ViewAll = () => {
 
     return (
         <>
-        <div className='bg-img'>
-            <div className='container d-block mx-auto w-50 my-3 p-3' >
-                <Link className="btn btn-secondary" to={'/chatter/add'}>Add New Post</Link>
-                <div className="row row-cols-2">
-                    <div className="col-sm-8">
+            <div className='bg-img'>
+                <div className='container d-block mx-auto w-50 my-3 p-3' >
+                    <Link className="btn btn-secondary" to={'/chatter/add'}>Add New Post</Link>
+                    <div className="row row-cols-2">
                         {loaded && chatters.map((chatter, key) =>
-                            <div key={key}>
-                                <div className="card">
-                                    <div className="card-body">
-                                        <img src={chatter.image || process.env.PUBLIC_URL + '/img-not-found.png'} class="img-fluid mx-auto d-block" alt={chatter.image} />
-                                        <h3 className="card-title fw-semibold"><Link to={`/chatter/${chatter._id}`}>{chatter.message}</Link></h3>
-                                        <Link className='btn btn-warning' to={`/chatter/update/${chatter._id}`}>Edit</Link>
-                                        <button className='btn btn-danger' onClick={() => deleteChatter(chatter._id)}>Delete</button>
-                                        <div class="dropdown">
-                                            <button className='btn btn-primary dropdown-toggle' type="button" data-bs-toggle="dropdown" aria-expanded="false">Comment</button>
-                                            <ul class="dropdown-menu">
-                                                {loaded && comment.map((comment, key) => 
-                                                    <div key={key}>
-                                                        <li>
-                                                            <p className="dropdown-item">{comment}</p>
-                                                            <hr/>
-                                                        </li>
-                                                    </div>
-                                                )}
-                                                <Link className="btn btn-secondary" to={`/chatter/update/${comment.id}`}>Make a Comment</Link>
-                                            </ul>
+                            <div className="col-6">
+                                <div key={key}>
+                                    <div className="card">
+                                        <div className="card-body">
+                                            <img src={chatter.image || process.env.PUBLIC_URL + '/img-not-found.png'} class="img-fluid mx-auto d-block" alt={chatter.image} />
+                                            <h3 className="card-title fw-semibold"><Link to={`/chatter/${chatter._id}`}>{chatter.message}</Link></h3>
+                                            <Link className='btn btn-warning' to={`/chatter/update/${chatter._id}`}>Edit</Link>
+                                            <button className='btn btn-danger' onClick={() => deleteChatter(chatter._id)}>Delete</button>
+                                            <div class="dropdown">
+                                                <button className='btn btn-primary dropdown-toggle' type="button" data-bs-toggle="dropdown" aria-expanded="false">Comment</button>
+                                                <ul class="dropdown-menu">
+                                                    {loaded && comment.map((comment, key) =>
+                                                        <div key={key}>
+                                                            <li>
+                                                                <p className="dropdown-item">{comment}</p>
+                                                                {/* <button className='btn btn-warning'>Edit</button>
+                                                            <button className='btn btn-danger'>Delete</button> */}
+                                                                <hr />
+                                                            </li>
+                                                        </div>
+                                                    )}
+                                                    <Link className="btn btn-secondary" to={`/chatter/update/${comment.id}`}>Make a Comment</Link>
+                                                </ul>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -67,7 +71,6 @@ const ViewAll = () => {
                     </div>
                 </div>
             </div>
-        </div>
         </>
     )
 }
